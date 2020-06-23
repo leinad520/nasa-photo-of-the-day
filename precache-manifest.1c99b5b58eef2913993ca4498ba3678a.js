1dfa9eab170261e8e0931a144a816f46c714cf3f
Daniel Sangyoon Kim <ksydaniel@gmail.com>
2020-06-23 01:00:12 -0700

    Updates

---
+++ precache-manifest.1c99b5b58eef2913993ca4498ba3678a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee9787133d038292e229a5011f17608",
+    "url": "/nasa-photo-of-the-day/index.html"
+  },
+  {
+    "revision": "60c95abe09ba1d6b4cd5",
+    "url": "/nasa-photo-of-the-day/static/css/main.fd079344.chunk.css"
+  },
+  {
+    "revision": "1a96f57811bd4f61c221",
+    "url": "/nasa-photo-of-the-day/static/js/2.cc0d54bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nasa-photo-of-the-day/static/js/2.cc0d54bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c95abe09ba1d6b4cd5",
+    "url": "/nasa-photo-of-the-day/static/js/main.c9505e8b.chunk.js"
+  },
+  {
+    "revision": "1029f2261087281c91c9",
+    "url": "/nasa-photo-of-the-day/static/js/runtime-main.82a31dc6.js"
+  },
+  {
+    "revision": "23d919aed562052a3766a158a8443331",
+    "url": "/nasa-photo-of-the-day/static/media/nasaLogo.23d919ae.png"
+  }
+]);
